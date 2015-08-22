a69daff36c30bf07704297b9e98106caad52b6b8
Puneet1993 <rajatsharma050893@gmail.com>
2015-08-22 20:51:32 +0530

    Second Commit

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Building : MonoBehaviour 
+{
+	[SerializeField] Text score;
+
+	[SerializeField] GameObject winText;
+	[SerializeField] GameController gameController;
+
+	[SerializeField] Transform doorManager;
+
+	[SerializeField] float points;
+	[SerializeField] float rotationValue;
+	[SerializeField] float delay;
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("call"+ other.tag);
+		if(other.tag == "Player")
+		{
+			doorManager.rotation = Quaternion.Euler(0,rotationValue,0);
+			StartCoroutine(Points());
+		}
+	} 
+
+	IEnumerator Points()
+	{
+		yield return new WaitForSeconds(0.5f);
+		winText.SetActive(true);
+		yield return new WaitForSeconds(delay);
+		gameController.score += points;
+		score.text = gameController.score.ToString()+ " Points";
+		yield return new WaitForSeconds(delay);
+		Application.LoadLevel(Application.loadedLevel);
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Player")
+		doorManager.rotation = Quaternion.Euler(0,0,0);
+	}
+}	
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	// detinations
+	[SerializeField] Transform building1;
+	[SerializeField] Transform building2;
+
+	Vector3 destinationPosition;
+
+	// agent
+	[SerializeField] NavMeshAgent agent = new NavMeshAgent();
+	[SerializeField] Transform agentTransform;
+
+	[SerializeField] float movingDistance;
+	public float score;
+
+	[SerializeField] GameObject p1button;
+	[SerializeField] GameObject p2button;
+
+	public void P1()
+	{
+		DeactivateButtons(false);
+		destinationPosition = new Vector3(building1.position.x,building1.position.y,building1.position.z + movingDistance);
+		agent.SetDestination(destinationPosition);
+	}
+
+	public void P2() 
+	{
+		DeactivateButtons(false);
+		destinationPosition = new Vector3(building2.position.x,building2.position.y,building2.position.z + movingDistance);
+		agent.SetDestination(destinationPosition);
+	} 
+
+	void DeactivateButtons(bool boolValue)
+	{
+		p1button.SetActive(boolValue);
+		p2button.SetActive(boolValue);
+	}
+
+	public void Restart()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
